3164a64a5c14d80d09469adf2fce494c93d65222
drmats <drmats@users.noreply.github.com>
2024-02-13 12:26:35 +0100

    common: sqlite/lib::setPragmas() - customized 'journal_mode' and 'synchronous'.

--- packages/common/src/sqlite/lib.ts
+++ packages/common/src/sqlite/lib.ts
@@ -11,15 +11,26 @@ import type { Database } from "better-sqlite3";
 /**
  * Set sane pragmas.
  */
-export const setPragmas = (db: Database): string[] => {
+export const setPragmas = (
+    db: Database,
+    opts?: {
+        journal_mode?:
+            "DELETE" | "TRUNCATE" | "PERSIST" | "MEMORY" | "WAL" | "OFF";
+        synchronous?:
+            "OFF" | "NORMAL" | "FULL" | "EXTRA";
+    },
+): string[] => {
+    const journal_mode = opts?.journal_mode ?? "DELETE";
+    const synchronous = opts?.synchronous ?? "NORMAL";
     const pragmas = [
         `busy_timeout = ${5000}`,
         `cache_size = ${2000}`,
         `foreign_keys = ${"ON"}`,
-        `journal_mode = ${"WAL"}`,
+        `journal_mode = ${journal_mode}`,
         `journal_size_limit = ${64 * 1024 * 1024}`,
+        `locking_mode = ${"NORMAL"}`,
         `mmap_size = ${128 * 1024 * 1024}`,
-        `synchronous = ${"FULL"}`,
+        `synchronous = ${synchronous}`,
     ];
     pragmas.forEach((setter) => db.pragma(setter));
     return pragmas;
